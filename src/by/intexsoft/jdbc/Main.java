f5fc07f7cbf33d3e4be08c2a3c5100a88c5b3417
zmzm <yandex6565@tut.by>
2015-07-30 16:37:41 +0300

    update

--- src/by/intexsoft/jdbc/Main.java
+++ src/by/intexsoft/jdbc/Main.java
@@ -1,13 +1,13 @@
 package by.intexsoft.jdbc;
 
 /**
- * Created by Admin on 24.07.2015.
+ * Created by Admin on 27.07.2015.
  */
 public class Main {
+
     public static void main(String[] args) {
         DataBase.createDB();
         DataBase.table("one");
         DataBase.table("two");
-
     }
 }
